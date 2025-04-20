105e1d3a6dccbef76f87dad25d8ff89072860049
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-19 22:48:29 -0300

    create router post/clientes - register clientes

--- src/controller/clientes.js
+++ src/controller/clientes.js
@@ -1,9 +1,29 @@
+const db = require('../connection/db_connection');
+
 const registerClient = async (req, res) => {
     const { nome, telefone, email, objetivo, endereço } = req.body;
 
     try {
-        return res.status(201).json({ Register: "register client sucesso!" })
+
+        const data = await db("clientes").insert({
+            nome,
+            telefone,
+            email,
+            objetivo,
+            endereço
+        }).returning("*");
+
+        if (!data) {
+            return res.status(401).json({message: "Ocorreu um erro"});
+        }
+
+        return res.status(201).json({ message: "register client sucesso!" })
+
     } catch (error) {
-        return res.status(400).json({ message: error.detail })
+        return res.status(400).json({ message: error })
     }
+};
+
+module.exports = {
+    registerClient
 };
