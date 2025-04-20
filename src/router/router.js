8c6cd5ea48ae7748134129abb08c6057f2da3df7
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-20 17:20:08 -0300

    add added client/registerClients route

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,8 @@
 const epxress = require("express");
 const router = epxress.Router();
 
+const { registerClient } = require("../controller/clientes");
 
+router.post("/clientes", registerClient);
 
 module.exports = router;
