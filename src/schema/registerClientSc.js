8a7a11a6c2e5dccd628d79275afcbbb6ed4c0194
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-20 20:52:11 -0300

    Joi schema validator update fix

--- src/schema/registerClientSc.js
+++ src/schema/registerClientSc.js
@@ -1,13 +1,22 @@
 const Joi = require("joi");
 
 const clientSchema = Joi.object({
-    
-    nome: Joi.string().trim().required(),
-    telefone: Joi.string().trim().required(),
-    email: Joi.string().email().trim().required(),
-    objetivo: Joi.string().trim().required(),
-    endereço: Joi.string().trim().required()
-
-})
+  nome: Joi.string().trim().required().messages({
+    "string.base": "O nome precisa ser um texto",
+    "string.empty": "Você precisa adicionar um nome",
+    "any.required": "O campo nome é obrigatório"
+  }),
+  telefone: Joi.string().trim().required().messages({
+    "string.empty": "O telefone é obrigatório",
+    "any.required": "O campo telefone é obrigatório"
+  }),
+  email: Joi.string().email().trim().required().messages({
+    "string.email": "O e-mail precisa ser válido",
+    "string.empty": "O e-mail não pode estar vazio",
+    "any.required": "O campo e-mail é obrigatório"
+  }),
+  objetivo: Joi.string().trim(),
+  endereço: Joi.string().trim(),
+});
 
 module.exports = clientSchema;
