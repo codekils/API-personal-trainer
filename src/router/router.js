e52480aa447d6f4c1b642e29570fbcf30284d979
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-21 22:59:32 -0300

    endpoint.post/agendamentos - appointment record

--- src/router/router.js
+++ src/router/router.js
@@ -6,14 +6,18 @@ const validatorToken = require("../middleware/validatorToken");
 
 // Import Routers
 const login = require("../controller/login");
+const { agendaRegister } = require("../controller/agendamentos");
 const { 
-    registerClient
+    registerClient,
+    listAgenda
 } = require("../controller/clientes");
 
 // Login ADMIN 
 route.post("/login", login)
 route.use(validatorToken);
 // Router Register Client
+route.get("/agendamentos", listAgenda);
+route.post("/agendamentos", agendaRegister);
 route.post("/clientes",  validateRegister,registerClient);
 
 module.exports = route;
