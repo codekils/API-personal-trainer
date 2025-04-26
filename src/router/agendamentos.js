1291d7b57ddfa1f36454f672ef1b84703c2576f0
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-25 22:07:33 -0300

    added listAgendaDay Route

--- src/router/agendamentos.js
+++ src/router/agendamentos.js
@@ -3,12 +3,14 @@ const router = epxress.Router();
 
 const { 
     agendaRegister,
+    listAgendaDay,
     listAgenda
  } = require("../controller/agendamentos");
 
 
 // Router Agendamentos
 router.get("/agendamentos", listAgenda);
+router.get("/agendamentos", listAgendaDay);
 router.post("/agendamentos", agendaRegister);
 
 module.exports = router;
