80f7e5e05350c5e1cba76188f776d486ac8f4ecb
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-26 22:16:45 -0300

    correction function name and added deleteClient

---
+++ src/controller/handlerClients.js
@@ -0,0 +1,55 @@
+const db = require('../connection/db_connection');
+
+// Cadastro de clientes
+const registerClient = async (req, res) => {
+    const { nome, telefone, email, objetivo, endereço } = req.body;
+
+    try {
+
+        const data = await db("clientes").insert({
+            nome,
+            telefone,
+            email,
+            objetivo,
+            endereço
+        }).returning("*");
+
+        if (!data) {
+            return res.status(401).json({message: "Ocorreu um erro"});
+        }
+
+        return res.status(201).json({ message: "register client sucesso!" })
+
+    } catch (error) {
+        return res.status(400).json({ message: error })
+    }
+};
+
+const deleteClient = async (req, res) => {
+    const { id } = req.query;
+    console.log(id);
+    
+    try {
+
+      const data = await db("clientes").where({id}).delete();
+        console.log(data);
+        
+      if (!data) {
+        return res.status(400).json({ message: "Usuário informado não foi encontrado!" });
+      };
+
+      return res.status(200).json({ message: "Cliente deletado com sucesso!" });
+      
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    };
+
+
+};
+
+
+
+module.exports = {
+    registerClient,
+    deleteClient
+};
