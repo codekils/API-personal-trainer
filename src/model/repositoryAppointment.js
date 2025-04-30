bd285a25b2bdc2926d107ef50f6553b083c3c75c
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-30 15:04:48 -0300

    feat(getByIdClient and registerAppoinment) adding features, data processing

--- src/model/repositoryAppointment.js
+++ src/model/repositoryAppointment.js
@@ -1,23 +1,20 @@
 const db = require("../connection/db_connection");
 
 class appointmentRepository {
+    // Atualiza um agendamento
     async update(id, dataToUpdate) {
-        try {            
+        try {
             const updateAppointment = await db("agendamentos")
-            .where({ id })
-            .update(dataToUpdate)
-            .returning("*");
+                .where({ id })
+                .update(dataToUpdate)
+                .returning("*");
             return updateAppointment;
         } catch (error) {
             console.error("Erro ao atualizar agendamento no repositório:", error);
             throw error;
         };
     };
-
-    async register(dataToUpdate) {
-
-    };
-    
+    // Busca agendamento pelo id do agendamento
     async getById(id) {
         try {
             const appointment = await db("agendamentos").where({ id }).first();
@@ -27,6 +24,28 @@ class appointmentRepository {
             throw error;
         };
     };
+// Busca cliente pelo id do cliente
+    async getByIdClient(cliente_id) {
+        try {
+            const client = await db("clientes").where({id:cliente_id}).first();
+            return client
+        } catch (error) {
+            console.error("Erro ao buscar cliente por ID no repositório", error.message)
+            throw error;
+        };
+    };
+    // Registra um agendamento
+    async registerAppointment(dataToUpdate) {
+        try {
+            const insertAppointment = await db("agendamentos").insert(dataToUpdate).returning("*");
+            return insertAppointment;
+        } catch (error) {
+            console.error("Erro ao registrar agendamento!", error.message);
+            throw error;
+        };
+    };
+
+
 };
 
 module.exports = new appointmentRepository;
