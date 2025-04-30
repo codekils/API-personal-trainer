5fe62550a6e69a5ed30ab2c4505718872856d60e
Jose Fernandes <fernandes.trbl@gmail.com>
2025-04-30 09:46:35 -0300

    Just correcting spaces

--- src/router/router.js
+++ src/router/router.js
@@ -7,13 +7,9 @@ const login = require("./login");
 const appointments = require("./appointments");
 const registerClient = require("./clients");
 
-
 router.use(login);
 router.use(validatorToken);
 router.use(appointments);
 router.use(registerClient);
-
-// Router Register Client
-
 
 module.exports = router;
