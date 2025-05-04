af7df7eb1e43322bfeb9571bcb0b06239ad0532c
Jose Fernandes <fernandes.trbl@gmail.com>
2025-05-04 15:46:19 -0300

    feat(client) função adicionada a class ClientRespository, deleteClient()

--- src/model/repositoryClient.js
+++ src/model/repositoryClient.js
@@ -22,24 +22,24 @@ class ClientRepository {
         } catch (error) {
             console.error("Erro ao buscar email e telefone no repositório:", error.message);
         };
-    }
+    };
 
     async getById(id) {
         try {
-            const getById = await db("clientes").where({ id }).first();
-            return getById
+            const getById = await db("clientes").where(id).first();
+            return getById;
         } catch (error) {
             console.error("Erro ao buscar cliente pelo ID no repositório:", error.message);
             throw error;
-        }
-    }
+        };
+    };
 
     async deleteClient(id) {
         try {
 
-            const deletedClient = await db("clientes").where({ id }).delete();
+            const deletedClient = await db("clientes").where(id).delete().returning("*");
             return deletedClient;
-            
+
         } catch (error) {
             console.error("Erro ao deletar o cliente do banco de dados!", error.message);
         };
