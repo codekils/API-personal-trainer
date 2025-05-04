8c7d57d5f7b5c7c066b18cc4e52ac869a8338e2b
Jose Fernandes <fernandes.trbl@gmail.com>
2025-05-04 14:02:11 -0300

    feat(client): implement ClientRepository to interact with the client database

--- src/model/repositoryClient.js
+++ src/model/repositoryClient.js
@@ -1,23 +1,26 @@
 const db = require("../connection/db_connection");
 
-class clientRepository {
+class ClientRepository {
 
-    async insert(newClientData) {
+    async registerClient(newClientData) {
         try {
+
             const registedClientData = await db("clientes").insert(newClientData).returning("*");
             return registedClientData;
+            
         } catch (error) {
-            console.error("Erro ao inserir cliente no repositório:", error);
+            console.error("Erro ao inserir cliente no repositório:", error.message);
         };
     };
 
-    async getByEmailPhone(email, phone){
+    async getByEmailPhone(email, telefone) {
         try {
-            const foundClient = await db("clientes").where({ email }).orWhere({telefone:phone}).first();
+
+            const foundClient = await db("clientes").where({ email }).orWhere({ telefone }).first();
             return foundClient;
 
         } catch (error) {
-            console.error("Erro ao buscar email e telefone no repositório:", error);
+            console.error("Erro ao buscar email e telefone no repositório:", error.message);
         };
     }
 
@@ -26,10 +29,10 @@ class clientRepository {
             const getById = await db("clientes").where({ id }).first();
             return getById
         } catch (error) {
-            console.error("Erro ao buscar cliente pelo ID no repositório:", error);
+            console.error("Erro ao buscar cliente pelo ID no repositório:", error.message);
             throw error;
         }
     }
 }
 
-module.exports = new clientRepository;
+module.exports = new ClientRepository;
