4c6f5b234d88a1a24b1f38c31ce751dc7e09f367
Jose Fernandes <fernandes.trbl@gmail.com>
2025-05-04 15:44:36 -0300

    feat(client) regras de negócios atribuida para clientService.js

--- src/controller/handlerClients.js
+++ src/controller/handlerClients.js
@@ -1,36 +1,27 @@
-const db = require('../connection/db_connection');
-const { registerClientService } = require("../services/clientService");
+const { registerClientService, deleteClientService } = require("../services/clientService");
 
 // Cadastro de clientes
 const registerClient = async (req, res) => {
-
   try {
-    
+
     const registedClientData = await registerClientService(req);
     return res.status(201).json({ message: "Cliente registrado com sucesso!", registedClientData });
 
   } catch (error) {
-    console.error("Usuário ou dados informados já existem");
+    console.error("Cliente ou dados informados já existem", error.message);
     return res.status(error.statusCode || 500).json({ error: error.message })
-
   };
 };
 // Deletar cliente pelo id
 const deleteClient = async (req, res) => {
-  const { id } = req.query;
-
   try {
 
-    const data = await db("clientes").where({ id }).delete();
-
-    if (!data) {
-      return res.status(400).json({ message: "Usuário informado não foi encontrado!" });
-    };
-
-    return res.status(200).json({ message: "Cliente deletado com sucesso!" });
+    const deletedClientDB = await deleteClientService(req);
+    return res.status(200).json({ message: "Cliente deletado com sucesso!", deletedClientDB });
 
   } catch (error) {
-    return res.status(400).json({ message: error });
+    console.error("Ocorreu um error ao deletar o cliente!", error.message);
+    return res.status(error.statusCode || 500).json({ error: error.message });
   };
 
 };
